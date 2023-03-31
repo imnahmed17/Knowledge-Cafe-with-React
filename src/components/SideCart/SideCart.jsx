350c608e31e10156aa7448fed63abe5c3b0ef724
helloashik17 <asiknoman150@gmail.com>
2023-03-31 21:41:09 +0600

    spent time rendered from localstorage

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './SideCart.css';
 
 const SideCart = ({ readTime }) => {
-    
+    const [time, setTime] = useState(readTime);
+
+    useEffect(() => {
+        const getReadTimeFromStorage = localStorage.getItem("readTime");
+        if (getReadTimeFromStorage) {
+            setTime(getReadTimeFromStorage);
+        } else {
+            setTime(readTime);
+        }
+    }, [readTime]);
 
     return (
         <div className='side-cart'>
             <div className='time-spent rounded py-3'>
-                <h6 className='fw-bold text-center'>Spent time on read: {readTime} min</h6>
+                <h6 className='fw-bold text-center'>Spent time on read: {time} min</h6>
             </div>
             <div className='bookmark-card p-3 my-3 rounded'>
                 <p className='fw-bold'>Bookmarked Blogs: </p>
+                
             </div>
         </div>
     );
