73d8fb1aeb97ae749bd328f6a5574c191d3a1212
helloashik17 <asiknoman150@gmail.com>
2023-04-01 13:38:35 +0600

    blog questions added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import SideCart from './components/SideCart/SideCart';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Blog from './components/Blog/Blog';
 
 function App() {
   const [readTime, setReadTime] = useState(0);
@@ -32,7 +33,7 @@ function App() {
     const previousBookmark = JSON.parse(localStorage.getItem("bookmarks"));
     
     if (previousBookmark && previousBookmark.includes(title)) {
-      toast("Already bookmarked!");
+      toast("You Have Already Bookmarked This Blog!");
     }
 
     localStorage.setItem("bookmarks", JSON.stringify([...bookmarkInfo, title]));
@@ -41,16 +42,19 @@ function App() {
 
   return (
     <div>
-      <div className='header m-auto mb-3'>
+      <div className='header m-auto'>
         <Header></Header>
       </div>
-      <div className='main row'>
+      <div className='main row my-4'>
         <div className='home-container col-md-8'>
           <Home handleReadTime={handleReadTime} handleBookmark={handleBookmark}></Home>
         </div>
         <div className='side-cart col-md-4'>
           <SideCart readTime={readTime} bookmarkInfo={bookmarkInfo}></SideCart>
         </div>
+      </div>
+      <div>
+        <Blog></Blog>
       </div>
       <ToastContainer></ToastContainer>
     </div>
